a170b87c462916ba3e7615e88e3400eed2eba4fe
DINUM-304427 <dinum-304427@MacBook-Air-de-DINUM-304427.local>
2025-01-24 18:14:53 +0100

    chore : scalingo configuration

--- aides-agri/settings/local.py
+++ aides-agri/settings/local.py
@@ -7,18 +7,6 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME', default='your_db_name'),
-        'USER': env('DB_USER', default='postgres'),
-        'PASSWORD': env('DB_PASSWORD', default=''),
-        'HOST': env('DB_HOST', default='localhost'),
-        'PORT': env('DB_PORT', default='5432'),
-    }
-}
-
-
 if DEBUG:
     INSTALLED_APPS += ['debug_toolbar', 'django_dump_die']
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', 'django_dump_die.middleware.DumpAndDieMiddleware']
