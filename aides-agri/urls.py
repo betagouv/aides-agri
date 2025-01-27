a787ed4d0e8fa7daddce9a38e01ee91eb44ad960
David Guillot <david.guillot@beta.gouv.fr>
2025-01-27 17:14:57 +0100

    fix(urls): load djDT only when DEBUG

--- aides-agri/urls.py
+++ aides-agri/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('', include('apps.pages.urls')),
@@ -25,4 +25,9 @@ urlpatterns = [
     path('questionnaire/', include('apps.questionnaire.urls')),
     path("catalog/", include("apps.catalog.urls")),
     path('chat', include('apps.chat.urls'))
-] + debug_toolbar_urls()
+]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()
