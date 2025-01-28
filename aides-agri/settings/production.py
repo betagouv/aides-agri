501265151aa6e8e43bb4966654f1c99aa774a142
David Guillot <david.guillot@beta.gouv.fr>
2025-01-28 16:48:09 +0100

    chore(scalingo): production settings
    * dj_database_url is a low-value dependency, let's get rid of it
    * SECRET_KEY is a generally required envvar since a previous commit
    * whitenoise hasn't been properly configured in a previous commit

--- aides-agri/settings/production.py
+++ aides-agri/settings/production.py
@@ -9,15 +9,18 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-
-# Database - Scalingo injecte automatiquement DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
 # Sécurité
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# Secret Key - à définir dans les variables d'environnement Scalingo
-SECRET_KEY = env('SECRET_KEY')
+# Whitenoise for static files
+MIDDLEWARE.insert(
+    MIDDLEWARE.index("django.middleware.security.SecurityMiddleware") + 1,
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+)
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
