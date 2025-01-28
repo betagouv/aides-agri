417446581edb5c45afee4daa265230c58c79ab42
David Guillot <david.guillot@beta.gouv.fr>
2025-01-28 14:48:42 +0100

    chore: cleaner settings + docker-compose
    Cleaner settings:
    * We don't need django-environ: the environment is in charge of providing envvars, not the application ; on local, `just` reads .env file ; on Scalingo, Scalingo reads envvars we define on the platform and passes them through whatever they want
    * SECRET_KEY will always be an envvar, and it's required, no default
    * DEBUG is always False except on local where it's always True ; no need for an envvar for that
    * LANGUAGE_CODE will always be "fr-fr"
    * DATABASES will always use PostgreSQL, whether on local or on Scalingo ; so I moved these settings to `base.py` and made it take sensible defaults
    
    This commit also adds a basic docker-compose.yml with a default PostgreSQL DB

--- aides-agri/settings/local.py
+++ aides-agri/settings/local.py
@@ -2,11 +2,14 @@ from .base import *
 
 DEBUG = True
 
-import environ
-env = environ.Env()
-environ.Env.read_env()
+INSTALLED_APPS += [
+    'debug_toolbar',
+    'django_dump_die'
+]
 
-if DEBUG:
-    INSTALLED_APPS += ['debug_toolbar', 'django_dump_die']
-    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', 'django_dump_die.middleware.DumpAndDieMiddleware']
-    INTERNAL_IPS = ['127.0.0.1']
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django_dump_die.middleware.DumpAndDieMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1']
