56235b47648813c61b17d73689d979a76c0ed717
David Guillot <david.guillot@beta.gouv.fr>
2025-04-09 18:15:54 +0200

    feat(aides): first scrapers

---
+++ apps/aides/scrapers/__init__.py
@@ -0,0 +1,15 @@
+from . import (
+    aides_entreprises,
+    aides_territoires,
+    eau_grandsudouest,
+    eau_loire_bretagne,
+    france_agrimer,
+)
+
+__all__ = [
+    "aides_entreprises",
+    "aides_territoires",
+    "eau_loire_bretagne",
+    "eau_grandsudouest",
+    "france_agrimer",
+]
---
+++ apps/aides/scrapers/eau_grandsudouest.py
@@ -0,0 +1,49 @@
+import logging
+from enum import Enum
+
+from markdownify import markdownify as md
+
+from ._common import get_soup_from_url, load_into_grist
+
+logger = logging.getLogger(__name__)
+scrapername = __name__.split(".")[-1]
+
+
+class Fields(Enum):
+    NOM = "nom"
+    DESCRIPTION_LONGUE = "description_longue"
+    DESCRIPTION_COURTE = "description_courte"
+    PROCEDURE = "procedure"
+
+
+def scrape():
+    soup = get_soup_from_url(
+        "https://eau-grandsudouest.fr/aides-financieres", verify_tls=False
+    )
+
+    to_load = []
+    for theme in soup.find_all(class_="thematique-term-item"):
+        soup = get_soup_from_url(theme.find("a").get("href"), verify_tls=False)
+        for found_aide in soup.find_all(
+            class_="paragraph--type--blocs-dropdown-enrichi"
+        ):
+            data = {key.value: "" for key in Fields}
+
+            nom = found_aide.find(class_="title-drop-down").string
+            data[Fields.NOM.value] = nom.strip() if nom else ""
+            data[Fields.DESCRIPTION_COURTE.value] = md(
+                str(found_aide.find(class_="col-lg-7"))
+            )
+            container1 = found_aide.find(class_="col-lg-5").find(
+                class_="container-push-1"
+            )
+            if container1:
+                data[Fields.DESCRIPTION_LONGUE.value] = md(str(container1))
+            container2 = found_aide.find(class_="col-lg-5").find(
+                class_="container-push-2"
+            )
+            if container2:
+                data[Fields.PROCEDURE.value] = md(str(container2))
+            to_load.append(data)
+
+    load_into_grist(scrapername, to_load)
