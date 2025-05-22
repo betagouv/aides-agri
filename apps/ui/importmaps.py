8fb99cf6670523a4770881922212c2225ca97922
David Guillot <david.guillot@beta.gouv.fr>
2025-05-22 07:23:14 +0200

    fix(ui): setup Sentry properly by vendorizing it

--- apps/ui/importmaps.py
+++ apps/ui/importmaps.py
@@ -2,6 +2,7 @@ from importmap import static
 
 
 importmaps = {
+    "sentry": static("vendor/sentry.js"),
     "stimulus": static("vendor/stimulus.js"),
     "dsfr-form": static("ui/controllers/dsfr_form.js"),
     "checkbox-group-field": static("ui/components/checkbox-group-field.js"),
