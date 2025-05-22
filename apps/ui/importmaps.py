bf953cf9feaeaffc5d410cf89bf0833d11fe3f29
David Guillot <david.guillot@beta.gouv.fr>
2025-05-22 07:18:09 +0200

    refactor(js): setup dj-importmap
    thus, delete all in-page importmaps

---
+++ apps/ui/importmaps.py
@@ -0,0 +1,9 @@
+from importmap import static
+
+
+importmaps = {
+    "stimulus": static("vendor/stimulus.js"),
+    "dsfr-form": static("ui/controllers/dsfr_form.js"),
+    "checkbox-group-field": static("ui/components/checkbox-group-field.js"),
+    "select-rich": static("ui/components/select-rich.js"),
+}
