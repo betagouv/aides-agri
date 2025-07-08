1bc0dfe6e7b248542a61ac903cf9ddd09f4dc392
David Guillot <david.guillot@beta.gouv.fr>
2025-07-08 16:46:45 +0200

    feat(aides): rich text editing with django-prose instead of markdownx

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     "django_otp.plugins.otp_totp",
     "django_tasks",
     "django_tasks.backends.database",
-    "markdownx",
+    "prose",
     "reversion",
     "two_factor",
     "widget_tweaks",
