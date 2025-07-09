557dd6f60a8b8f7b81ce764e575e995e30d86845
David Guillot <david.guillot@beta.gouv.fr>
2025-07-09 15:43:43 +0200

    feat(deps): setup django-pgtrigger

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django_otp.plugins.otp_totp",
     "django_tasks",
     "django_tasks.backends.database",
+    "pgtrigger",
     "prose",
     "reversion",
     "two_factor",
