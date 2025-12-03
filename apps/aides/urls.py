9bf1b76ec84eea399589ac87f57ca8afb65d434a
David Guillot <david.guillot@beta.gouv.fr>
2025-12-03 16:32:24 +0100

    feat(aides): new Parent Aide page

--- apps/aides/urls.py
+++ apps/aides/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import AideDetailView
+from .views import AideDetailView, ParentAideDetailView
 
 
 app_name = "aides"
 urlpatterns = [
     path("aide/<int:pk>-<str:slug>", AideDetailView.as_view(), name="aide"),
+    path(
+        "ensemble-d-aides/<int:pk>-<str:slug>",
+        ParentAideDetailView.as_view(),
+        name="parent-aide",
+    ),
 ]
