11200106f11794a3e820e207d8d0ac54fc6d3b9d
David Guillot <david.guillot@beta.gouv.fr>
2025-12-10 14:11:50 +0100

    chore(aides): delete dead code

--- apps/aides/templatetags/aides_tags.py
+++ apps/aides/templatetags/aides_tags.py
@@ -10,19 +10,6 @@ register = template.Library()
 
 
 @register.simple_tag()
-def aides_by_sujet_and_type_and_departement(
-    aides: dict[str, dict[str, dict[str, list[str]]]],
-    sujet: str,
-    type_aide: str,
-    departement: str,
-) -> str:
-    found = aides[sujet][type_aide][departement]
-    return (
-        mark_safe("<ul><li>" + "</li><li>".join(found) + "</li></ul>") if found else ""
-    )
-
-
-@register.simple_tag()
 def readonly_hstore_field_or_contents(
     readonlyfield: AdminReadonlyField, original: Model
 ):
