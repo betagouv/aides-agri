13ba6a9e55d69f93cef603bd6987d86ac0945cdb
David Guillot <david.guillot@beta.gouv.fr>
2025-12-15 10:55:55 +0100

    feat(aides): Aides link-checker script now sends fewer mails

--- conf/settings/apps/aides.py
+++ conf/settings/apps/aides.py
@@ -1,5 +1,5 @@
 import os
 
-AIDES_MANAGERS = ("aides-agri@beta.gouv.fr",)
+AIDES_MANAGERS = os.getenv("AIDES_MANAGERS", "aides-agri@beta.gouv.fr").split(",")
 
 AIDES_AIDES_TERRITOIRES_API_KEY = os.getenv("AIDES_AIDES_TERRITOIRES_API_KEY", "")
