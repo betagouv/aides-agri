ca35d8ac93ea98fb3c26a977d699ca2e5f8e14e8
David Guillot <david.guillot@beta.gouv.fr>
2026-01-19 13:46:28 +0100

    test(agri_v2): minimal coverage

--- apps/agri_v2/tests/test_views.py
+++ apps/agri_v2/tests/test_views.py
@@ -4,9 +4,19 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_home(client, theme):
+def test_home(client, theme_urgence):
     # WHEN requesting home page
-    url = reverse("agri:home")
+    url = reverse("agri_v2:home")
+    response = client.get(url)
+
+    # THEN it's a 200
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_results(client):
+    # WHEN requesting home page
+    url = reverse("agri_v2:results")
     response = client.get(url)
 
     # THEN it's a 200
