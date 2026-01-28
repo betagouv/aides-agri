a520553b60e3fc740e03c8c631df5f1b7c7eac6b
David Guillot <david.guillot@beta.gouv.fr>
2026-01-28 11:00:22 +0100

    feat(ui): setup a conditional "demo site" modal

--- apps/ui/context_processors.py
+++ apps/ui/context_processors.py
@@ -14,10 +14,19 @@ def build_uri_for_qr_code(request):
 def ui_tools_tokens(request):
     qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage, box_size=7)
     qr.add_data(build_uri_for_qr_code(request))
-    return {
+    context_data = {
         "qrcode": qr.make_image().to_string(encoding="unicode"),
         "UI_ENVIRONMENT": settings.ENVIRONMENT,
         "UI_MATOMO_SITE_ID": settings.MATOMO_SITE_ID,
         "UI_SENTRY_PUBLIC_KEY": settings.SENTRY_UI_PUBLIC_KEY,
         "UI_SENTRY_PROJECT_ID": settings.SENTRY_UI_PROJECT_ID,
     }
+    if settings.UI_DEMO_SITE_MODAL_COOKIE_NAME:
+        context_data.update(
+            {
+                "UI_SHOW_DEMO_SITE_MODAL": True,
+                "UI_OPEN_DEMO_SITE_MODAL": settings.UI_DEMO_SITE_MODAL_COOKIE_NAME
+                not in request.COOKIES,
+            }
+        )
+    return context_data
