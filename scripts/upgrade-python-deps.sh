7de54858164c04a79b5eb6ef3f7758ebe9747f90
David Guillot <david.guillot@beta.gouv.fr>
2026-02-16 09:34:58 +0000

    Amélioration du script de mises à jour (#381)
    * chore(deps): setup uv cooldown
    
    * chore(deps): downgrade markdown according to cooldown
    
    * chore(scripts): better dependencies upgrade script

--- scripts/upgrade-python-deps.sh
+++ scripts/upgrade-python-deps.sh
@@ -2,23 +2,36 @@
 
 set -e
 
-regex="Update ([^ ]+) ([^ ]+) -> ([^ ]+)"
+regex="Update ([^ ]+) v([^ ]+) -> v([^ ]+)"
 OLDIFS=$IFS
 IFS=$'\n'
 
+echo "First, fetch origin..."
+git fetch origin --quiet
+echo "... done"
+
+echo "Then, check which packages uv wants to upgrade..."
 for update in $(uv lock --upgrade --dry-run 2>&1 |grep 'Update '); do
     if [[ "$update" =~ $regex ]]; then
         dep="${BASH_REMATCH[1]}"
         from_version="${BASH_REMATCH[2]}"
         to_version="${BASH_REMATCH[3]}"
-        slugified=$(echo "$dep/$from_version-$to_version" | sed -E 's/[^a-zA-Z0-9]+/-/g')
-        branch="chore/bump_deps/$slugified"
-        git switch -c "$branch"
+        slugified_dep=$(echo "$dep" | sed -E 's/[^a-zA-Z0-9]+/-/g')
+        dependabot_branch_pattern="dependabot/uv/$slugified_dep-$to_version"
+        echo "-> Found $dep to be bumped from $from_version to $to_version..."
+        if [[ -n `git branch -a |grep "$dependabot_branch_pattern"` ]]; then
+            echo "... ignoring because Dependabot seems to already have opened a PR about it."
+            continue
+        fi
+        branch="dependencies/$slugified_dep-$from_version-$to_version"
+        echo "... pushing branch $branch for that..."
+        git switch -c "$branch" --quiet
         uv lock --quiet --upgrade-package "$dep"
         git add uv.lock
-        git commit -m "feat(deps): Bump $dep from $from_version to $to_version"
-        git push -u origin "$branch"
-        git switch main
+        git commit --quiet -m "feat(deps): Bump $dep from $from_version to $to_version"
+        git push --quiet -u origin "$branch"
+        echo "... done."
+        git switch main --quiet
     fi
 done
 
