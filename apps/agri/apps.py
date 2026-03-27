e401caa910b86a30a051853655314c5806110eb5
David Guillot <david.guillot@beta.gouv.fr>
2026-03-27 12:42:19 +0000

    Obfusquer les données personnelles dans la base juste après avoir envoyé un mail (#439)
    * feat(agri): obfuscate PII in task results just after sending e-mail

--- apps/agri/apps.py
+++ apps/agri/apps.py
@@ -6,5 +6,8 @@ class AgriConfig(AppConfig):
     verbose_name = "Parcours agri"
 
     def ready(self):
+        # signal handlers
+        from .signals import handlers  # noqa: F401
+
         # ensure siret NAF codes mapping is run at application startup
-        from . import siret  # noqa
+        from . import siret  # noqa: F401
---
+++ apps/agri/tests/test_signal_handlers.py
@@ -0,0 +1,61 @@
+import pytest
+from django.test import override_settings
+from django_tasks.base import TaskResultStatus
+from django_tasks.backends.database.models import DBTaskResult
+from django_tasks.signals import task_finished
+
+from agri.tasks import send_results_by_mail
+
+
+@pytest.mark.django_db
+@override_settings(
+    TASKS={"default": {"BACKEND": "django_tasks.backends.database.DatabaseBackend"}}
+)
+def test_obfuscate_send_results_by_mail_pii():
+    # GIVEN a DBTaskResult for task send_results_by_mail
+    db_task_result = DBTaskResult.objects.create(
+        backend_name=send_results_by_mail.get_backend().alias,
+        task_path=send_results_by_mail.module_path,
+        status=TaskResultStatus.SUCCEEDED,
+        args_kwargs={"args": [], "kwargs": {"email": "name@domain.tld", "a": "bla"}},
+    )
+
+    # WHEN tasks is signaled as finished
+    task_finished.send(
+        sender=db_task_result.task_result.task.get_backend(),
+        task_result=db_task_result.task_result,
+    )
+
+    # THEN e-mail address has been redacted
+    db_task_result.refresh_from_db()
+    assert db_task_result.args_kwargs == {
+        "args": [],
+        "kwargs": {"email": "***REDACTED***", "a": "bla"},
+    }
+
+
+@pytest.mark.django_db
+@override_settings(
+    TASKS={"default": {"BACKEND": "django_tasks.backends.database.DatabaseBackend"}}
+)
+def test_does_not_obfuscate_send_results_by_mail_pii_when_failed():
+    # GIVEN a DBTaskResult for task send_results_by_mail
+    db_task_result = DBTaskResult.objects.create(
+        backend_name=send_results_by_mail.get_backend().alias,
+        task_path=send_results_by_mail.module_path,
+        status=TaskResultStatus.FAILED,
+        args_kwargs={"args": [], "kwargs": {"email": "name@domain.tld", "a": "bla"}},
+    )
+
+    # WHEN tasks is signaled as finished
+    task_finished.send(
+        sender=db_task_result.task_result.task.get_backend(),
+        task_result=db_task_result.task_result,
+    )
+
+    # THEN e-mail address has been redacted
+    db_task_result.refresh_from_db()
+    assert db_task_result.args_kwargs == {
+        "args": [],
+        "kwargs": {"email": "name@domain.tld", "a": "bla"},
+    }
