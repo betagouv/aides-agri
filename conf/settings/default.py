220bf63f57251649fed517550571f415e530908b
David Guillot <david.guillot@beta.gouv.fr>
2026-04-14 13:55:07 +0200

    deps: setup django-rest-framework

--- conf/settings/default.py
+++ conf/settings/default.py
@@ -5,6 +5,7 @@ from .apps.axes import *  # noqa
 from .apps.csp import *  # noqa
 from .apps.dsfr import *  # noqa
 from .apps.matomo import *  # noqa
+from .apps.rest_framework import *  # noqa
 from .apps.sentry import *  # noqa
 
 from .apps.agri import *  # noqa
