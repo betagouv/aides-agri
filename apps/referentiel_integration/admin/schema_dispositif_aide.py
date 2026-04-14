59ba69d5b39bd8d23ac6cf344f53d035743bb56d
David Guillot <david.guillot@beta.gouv.fr>
2026-04-14 14:10:57 +0200

    feat(referentiel_integration): dedicated Django app for integrating Demarches from various sources

---
+++ apps/referentiel_integration/admin/schema_dispositif_aide.py
@@ -0,0 +1,54 @@
+from admin_extra_buttons.api import ExtraButtonsMixin, button
+from django_auto_admin_fieldsets.admin import AutoFieldsetsMixin
+from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import reverse
+from reversion.admin import VersionAdmin
+
+from ..adapters import schema_dispositif_aide
+from ..models.schema_dispositif_aide import RawDemarcheSchemaDispositifAide
+
+
+@admin.register(RawDemarcheSchemaDispositifAide)
+class RawDemarcheSchemaAideAdmin(AutoFieldsetsMixin, ExtraButtonsMixin, VersionAdmin):
+    def get_model_perms(self, request):
+        return {
+            "add": False,
+            "change": False,
+            "delete": self.has_view_permission(request),
+            "view": self.has_view_permission(request),
+        }
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    fieldsets = [
+        ("Métadonnées", {"fields": ["source", "status", "demarche"]}),
+        ("Données", {"fields": ["__remaining__"]}),
+    ]
+
+    @button(
+        label="Intégrer dans le référentiel",
+        visible=lambda widget: widget.context["original"].is_new,
+        html_attrs={"class": "addlink"},
+    )
+    def integrate(self, request, object_id):
+        raw_demarche = self.get_object(request, object_id)
+        demarche = schema_dispositif_aide.create_demarche_agricole(raw_demarche)
+        return redirect(
+            reverse("admin:referentiel_demarcheagricole_change", args=[demarche.pk])
+        )
+
+    @button(
+        label="Modifier dans le référentiel",
+        visible=lambda widget: widget.context["original"].is_updated,
+        html_attrs={"class": "addlink"},
+    )
+    def edit(self, request, object_id):
+        return redirect(reverse("admin:referentiel_demarcheagricole_change", object_id))
---
+++ apps/referentiel_integration/models/schema_dispositif_aide.py
@@ -0,0 +1,31 @@
+import reversion
+from django.db import models
+
+from .base import RawDemarche, RawDemarcheFromExternalSource
+
+
+@reversion.register()
+class RawDemarcheSchemaDispositifAide(RawDemarcheFromExternalSource, RawDemarche):
+    class Meta:
+        verbose_name = "Démarche brute au schéma interministériel des aides"
+        verbose_name_plural = "Démarches brutes au schéma interministériel des aides"
+        unique_together = ("source", "id_externe")
+
+    id_externe = models.CharField()
+    titre = models.CharField()
+    promesse = models.CharField(blank=True)
+    description = models.TextField(blank=True)
+    eligibilite = models.TextField(blank=True)
+    types_aides = models.CharField(blank=True)
+    porteurs = models.JSONField(null=True, blank=True)
+    programmes_parents = models.CharField(blank=True)
+    url_source = models.URLField(blank=True)
+    cibles = models.CharField(blank=True)
+    eligibilite_geographique = models.CharField(blank=True)
+    eligibilite_geographique_exclusions = models.CharField(blank=True)
+    date_ouverture = models.DateField(null=True, blank=True)
+    date_cloture = models.DateField(null=True, blank=True)
+    date_mise_a_jour = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"[{self.source}] {self.titre}"
