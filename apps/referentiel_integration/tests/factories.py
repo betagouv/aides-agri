82d45bd104bf66bb67fcef79307f8beabb3de4ef
David Guillot <david.guillot@beta.gouv.fr>
2026-04-14 14:18:34 +0200

    feat(referentiel_integration): adapter to create referentiel.Demarche from RawDemarcheSchemaDispositifAide

---
+++ apps/referentiel_integration/tests/factories.py
@@ -0,0 +1,40 @@
+import factory
+
+from ..models import RawDemarcheSchemaDispositifAide
+
+
+class RawDemarcheSchemaDispositifAideFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RawDemarcheSchemaDispositifAide
+
+    titre = factory.Sequence(lambda n: f"Raw Demarche {n}")
+    promesse = factory.Sequence(lambda n: f"Promesse {n}")
+    description = factory.Sequence(lambda n: f"Description {n}")
+    eligibilite = factory.Sequence(lambda n: f"Conditions d’éligibilité {n}")
+    types_aides = ""
+    porteurs = ""
+    programmes_parents = ""
+    referents_internes = ""
+    url_source = ""
+    cibles = ""
+    eligibilite_geographique = ""
+    eligibilite_geographique_exclusions = ""
+    date_ouverture = None
+    date_cloture = None
+    date_mise_a_jour = None
+
+    # schema étendu : entreprises
+    base_juridique = []
+    ciblage_secteur_activite = ""
+    ciblage_naf = ""
+    ciblage_naf_exclusions = ""
+    eligibilite_effectif_minimal = None
+    eligibilite_effectif_maximal = None
+    eligibilite_annees_existence_minimal = None
+    eligibilite_forme_juridique = ""
+    eligibilite_forme_juridique_exclusions = ""
+    eligibilite_categorie_taille_entreprise = ""
+    chainage_paiement = ""
+
+
+__all__ = [RawDemarcheSchemaDispositifAideFactory]
---
+++ apps/referentiel_integration/tests/test_adapter_schema_dispositif_aide.py
@@ -0,0 +1,85 @@
+import pytest
+
+from referentiel.models import Demarche, Porteur
+
+from referentiel_integration.adapters.schema_dispositif_aide import (
+    SchemaDispositifAideIntegrationAdapter,
+)
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "raw_demarche_schema_dispositif_aide__cibles", ["professionnels"]
+)
+@pytest.mark.parametrize(
+    "raw_demarche_schema_dispositif_aide__programmes_parents", ["France 2030"]
+)
+@pytest.mark.parametrize(
+    "raw_demarche_schema_dispositif_aide__types_aides", ["financement|formation"]
+)
+@pytest.mark.parametrize(
+    "raw_demarche_schema_dispositif_aide__eligibilite_geographique", ["REG-75|DEP-13"]
+)
+@pytest.mark.parametrize(
+    "raw_demarche_schema_dispositif_aide__porteurs",
+    [[{"nom": "FranceAgriMer", "siren": "", "roles": ["financeur"]}]],
+)
+@pytest.mark.parametrize(
+    "raw_demarche_schema_dispositif_aide__base_juridique",
+    [
+        [
+            {
+                "libelle": "Décision n° 123456",
+                "lien": "https://www.franceagrimer.fr/decision-123456",
+            }
+        ]
+    ],
+)
+def test_create_demarche_from_raw_demarche_schema_dispositif_aide(
+    raw_demarche_schema_dispositif_aide,
+    organisme_fam,
+    programme_france_2030,
+    territoire_reg75,
+    territoire_dep13,
+):
+    # GIVEN no Demarche, and a RawDemarcheSchemaDispositifAide
+    assert not Demarche.objects.exists()
+
+    # WHEN integrating
+    adapter = SchemaDispositifAideIntegrationAdapter()
+    adapter.create_demarche(raw_demarche_schema_dispositif_aide)
+
+    # THEN a Demarche has been created
+    assert Demarche.objects.count() == 1
+    # no error has been raised
+    assert not adapter.errors
+    demarche = Demarche.objects.first()
+    assert demarche.titre == raw_demarche_schema_dispositif_aide.titre
+    # cibles have been properly identified and mapped
+    assert demarche.cibles == Demarche.Cible.PROFESSIONNELS
+    # programmes have been identified and mapped
+    assert demarche.programmes_parents.count() == 1
+    assert demarche.programmes_parents.first() == programme_france_2030
+    # types have been properly identified and mapped
+    assert demarche.types_aides == Demarche.Type.FINANCEMENT | Demarche.Type.FORMATION
+    # Porteurs have been properly created
+    assert demarche.porteurs.count() == 1
+    porteur = demarche.porteurs.first().porteur_set.first()
+    assert porteur.organisme == organisme_fam
+    assert porteur.roles == Porteur.Role.FINANCEUR
+    # Territoires have been properly linked
+    assert set(demarche.eligibilite_geographique.all()) == {
+        territoire_reg75,
+        territoire_dep13,
+    }
+    # BaseJuridique objects have been properly created
+    assert demarche.bases_juridiques.count() == 1
+    base_juridique = demarche.bases_juridiques.first()
+    assert (
+        base_juridique.libelle
+        == raw_demarche_schema_dispositif_aide.base_juridique[0]["libelle"]
+    )
+    assert (
+        base_juridique.url
+        == raw_demarche_schema_dispositif_aide.base_juridique[0]["lien"]
+    )
