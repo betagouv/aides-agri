53454003a69113af01068310817ef7e08accebdb
David Guillot <david.guillot@beta.gouv.fr>
2026-05-06 10:46:49 +0200

    refactor(ui): delete unused component checkbox-group-field

--- apps/ui/importmaps.py
+++ apps/ui/importmaps.py
@@ -7,6 +7,5 @@ importmaps = {
     "alert": static("ui/controllers/alert.js"),
     "dsfr-form": static("ui/controllers/dsfr_form.js"),
     "matomo": static("ui/controllers/matomo.js"),
-    "checkbox-group-field": static("ui/components/checkbox-group-field.js"),
     "select-rich": static("ui/components/select-rich.js"),
 }
