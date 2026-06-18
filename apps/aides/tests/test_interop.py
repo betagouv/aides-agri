d921addf5a8b011f36b2403c8ea5ad55ca34873b
David Guillot <david.guillot@beta.gouv.fr>
2026-06-18 07:48:09 +0200

    fix(aides): CSV export was working accidentally

--- apps/aides/tests/test_interop.py
+++ apps/aides/tests/test_interop.py
@@ -2,44 +2,44 @@ import csv
 import io
 import pytest
 
-from aides.interop import write_aides_as_csv
+from aides.interop import write_aides_as_csv, AideToSchema
 
 
 @pytest.mark.django_db
 def test_write_aides_as_csv(aide):
     with io.StringIO("") as f:
-        write_aides_as_csv(f, [aide.pk])
+        write_aides_as_csv(f, AideToSchema, [aide.pk])
         f.flush()
         f.seek(0)
         for i, row in enumerate(csv.reader(f)):
             if i == 0:
                 assert row == [
                     "id",
-                    "parent",
                     "titre",
                     "promesse",
                     "description",
                     "eligibilite",
                     "types_aides",
                     "porteurs",
-                    "porteur_principal_sous_famille",
                     "programmes_parents",
                     "url_source",
                     "cibles",
-                    "filieres",
-                    "sujets",
                     "eligibilite_geographique",
                     "eligibilite_geographique_exclusions",
-                    "est_une_declinaison_territoriale",
                     "date_ouverture",
                     "date_cloture",
                     "date_mise_a_jour",
-                    "etat",
-                    "est_publiee",
-                    "raison_desactivation",
-                    "url_site",
-                    "url_bureau_valideur",
-                    "url_bo",
+                    "base_juridique",
+                    "eligibilite_effectif_minimal",
+                    "eligibilite_effectif_maximal",
+                    "eligibilite_categorie_taille_entreprise",
+                    "eligibilite_annees_existence_minimal",
+                    "eligibilite_forme_juridique",
+                    "eligibilite_forme_juridique_exclusions",
+                    "ciblage_secteur_activite",
+                    "ciblage_naf",
+                    "ciblage_naf_exclusions",
+                    "chainage_paiement",
                 ]
             elif i == 1:
                 assert len(row) == 26
