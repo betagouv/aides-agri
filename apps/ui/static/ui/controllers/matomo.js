bc7de06e15517c65ae49304eb7b97f4f73838bf0
David Guillot <david.guillot@beta.gouv.fr>
2026-06-19 16:17:36 +0200

    feat(ui): Matomo controller, handle click tracking within dynamic content

--- apps/ui/static/ui/controllers/matomo.js
+++ apps/ui/static/ui/controllers/matomo.js
@@ -1,8 +1,18 @@
 import { Controller } from "stimulus"
 
 export class Matomo extends Controller {
+  static targets = ["trackableContent"]
+
   trackEvent(evt) {
     var _paq = window._paq = window._paq || []
     _paq.push(['trackEvent', evt.target.dataset.eventPage, evt.target.dataset.eventName])
+  }
+
+  trackableContentTargetConnected(elt) {
+    elt.querySelectorAll("a[target=_blank]").forEach(link => {
+      link.dataset.eventPage = elt.dataset.matomoTrackableContentEventPage
+      link.dataset.eventName = elt.dataset.matomoTrackableContentEventName
+      link.addEventListener("click", evt => this.trackEvent(evt))
+    })
   }
 }
