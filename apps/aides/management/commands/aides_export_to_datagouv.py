997f5d3cd84bb0d03d807cb21c912d83bc64f6f3
David Guillot <david.guillot@beta.gouv.fr>
2026-08-05 16:12:55 +0200

    fix(aides): minor adjustments to datagouv export command

--- apps/aides/management/commands/aides_export_to_datagouv.py
+++ apps/aides/management/commands/aides_export_to_datagouv.py
@@ -1,9 +1,10 @@
-import argparse
+import copy
 import datetime
 
 from datagouv import Client
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.utils.formats import date_format
 
 from ...models import Aide
 from ...interop import AideToSchema, write_aides_as_csv, AideToExternalSchema
@@ -16,7 +17,7 @@ class Command(BaseCommand):
         "organization": settings.AIDES_DATAGOUV_ORGANIZATION_ID,
     }
     RESOURCE_FOR_SCHEMA_PAYLOAD = {
-        "title": "Aides publiées sur Aides Agri, selon le schéma interministériel des aides aux enteprises",
+        "title": "Aides publiées sur Aides Agri, selon le schéma interministériel des aides aux entreprises",
         "schema": {
             "name": "etalab/dispositif-aide-professionnels",
             "url": "https://schema.data.gouv.fr/etalab/dispositif-aide-professionnels/",
@@ -51,7 +52,9 @@ class Command(BaseCommand):
             self.stdout.write(f"Resource {resource_id} created!")
         else:
             resource_id = getattr(settings, resource_id_setting)
-            self.client.resource(resource_id).update({}, file_to_upload=filename)
+            self.client.resource(resource_id).update(
+                resource_create_payload, file_to_upload=filename
+            )
             self.stdout.write(f"Resource {resource_id} updated with new file!")
 
     def __init__(self, **kwargs):
@@ -63,14 +66,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--write",
-            action=argparse.BooleanOptionalAction,
-            default=True,
-            help="Generate files but perform no write operations on data.gouv.fr.",
+            "--dry-run",
+            action="store_true",
+            default=False,
+            help="Set this flag to generate files but perform no write operations on data.gouv.fr.",
         )
 
-    def handle(self, *args, **options):
-        write = options.get("--write")
+    def handle(self, *args, dry_run: bool, **options):
+        write = not dry_run
+        today_iso = datetime.date.today().isoformat()
+        today_formatted = date_format(datetime.date.today(), "SHORT_DATE_FORMAT")
+
+        if not write:
+            self.stdout.write("DRY RUN MODE")
 
         # ensure dataset exists
         if write and not settings.AIDES_DATAGOUV_DATASET_ID:
@@ -93,24 +101,30 @@ class Command(BaseCommand):
         else:
             dataset_id = settings.AIDES_DATAGOUV_DATASET_ID
 
-        filename = f"/tmp/aides-agri-schema-dispositif-aide-professionnels-{datetime.date.today().isoformat()}.csv"
+        filename = (
+            f"/tmp/aides-agri-schema-dispositif-aide-professionnels-{today_iso}.csv"
+        )
         self._generate_file(AideToSchema, filename)
+        self.stdout.write(f"File {filename} created!")
         if write:
+            payload = copy.copy(self.__class__.RESOURCE_FOR_SCHEMA_PAYLOAD)
+            payload["title"] += f", au {today_formatted}"
             self._create_or_update_resource(
                 dataset_id,
                 "AIDES_DATAGOUV_RESOURCE_ID_FOR_SCHEMA",
-                self.__class__.RESOURCE_FOR_SCHEMA_PAYLOAD,
+                payload,
                 filename,
             )
 
-        filename = (
-            f"/tmp/aides-agri-schema-complet-{datetime.date.today().isoformat()}.csv"
-        )
+        filename = f"/tmp/aides-agri-schema-complet-{today_iso}.csv"
         self._generate_file(AideToExternalSchema, filename)
+        self.stdout.write(f"File {filename} created!")
         if write:
+            payload = copy.copy(self.__class__.RESOURCE_FOR_CUSTOM_PAYLOAD)
+            payload["title"] += f", au {today_formatted}"
             self._create_or_update_resource(
                 dataset_id,
                 "AIDES_DATAGOUV_RESOURCE_ID_FOR_CUSTOM",
-                self.__class__.RESOURCE_FOR_CUSTOM_PAYLOAD,
+                payload,
                 filename,
             )
