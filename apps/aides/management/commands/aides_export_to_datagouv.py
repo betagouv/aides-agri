22c0e2d1c568375e5b9b36856b21bb73111b7282
David Guillot <david.guillot@beta.gouv.fr>
2026-09-15 16:40:37 +0200

    feat(aides): datagouv export for Excel users

--- apps/aides/management/commands/aides_export_to_datagouv.py
+++ apps/aides/management/commands/aides_export_to_datagouv.py
@@ -8,7 +8,12 @@ from django.core.management.base import BaseCommand
 from django.utils.formats import date_format
 
 from ...models import Aide
-from ...interop import AideToSchema, write_aides_as_csv, AideToExternalSchema
+from ...interop import (
+    AideToSchema,
+    write_aides_as_csv,
+    AideToExternalSchema,
+    AideToExternalSchemaForHumans,
+)
 
 
 class Command(BaseCommand):
@@ -27,6 +32,9 @@ class Command(BaseCommand):
     }
     RESOURCE_FOR_CUSTOM_PAYLOAD = {
         "title": "Aides publiées sur Aides Agri, selon un schéma détaillé et exhaustif",
+    }
+    RESOURCE_FOR_EXCEL_PAYLOAD = {
+        "title": "Aides publiées sur Aides Agri, dans un format peut-être optimisé pour la lecture via Microsoft Excel",
     }
 
     def _generate_file(
@@ -131,6 +139,24 @@ class Command(BaseCommand):
             self._create_or_update_resource(
                 dataset_id,
                 "AIDES_DATAGOUV_RESOURCE_ID_FOR_CUSTOM",
+                payload,
+                filename,
+            )
+
+        filename = f"/tmp/aides-agri-excel-{today_iso}.csv"
+        self._generate_file(
+            AideToExternalSchemaForHumans,
+            filename,
+            encoding="iso8859_15",
+            delimiter=";",
+        )
+        self.stdout.write(f"File {filename} created!")
+        if write:
+            payload = copy.copy(self.__class__.RESOURCE_FOR_EXCEL_PAYLOAD)
+            payload["title"] += f", au {today_formatted}"
+            self._create_or_update_resource(
+                dataset_id,
+                "AIDES_DATAGOUV_RESOURCE_ID_FOR_EXCEL",
                 payload,
                 filename,
             )
--- apps/aides/tests/test_interop.py
+++ apps/aides/tests/test_interop.py
@@ -1,11 +1,17 @@
 import csv
 import io
 import json
+import tempfile
 
 import pytest
 from pytest_factoryboy import LazyFixture
 
-from aides.interop import write_aides_as_csv, AideToSchema, AideToExternalSchema
+from aides.interop import (
+    write_aides_as_csv,
+    AideToSchema,
+    AideToExternalSchema,
+    AideToExternalSchemaForHumans,
+)
 
 
 @pytest.mark.django_db
@@ -105,3 +111,70 @@ def test_write_aides_as_csv_with_external_schema(aide):
                 assert row[0] == str(aide.pk)
             else:
                 assert False
+
+
+@pytest.mark.django_db
+def test_write_aides_as_csv_with_humans_schema(aide_published):
+    aide = aide_published
+    with io.StringIO("") as f:
+        write_aides_as_csv(f, AideToExternalSchemaForHumans, [aide.pk])
+        f.flush()
+        f.seek(0)
+        for i, row in enumerate(csv.reader(f)):
+            if i == 0:
+                assert row == [
+                    "id",
+                    "titre",
+                    "promesse",
+                    "description",
+                    "montant",
+                    "participation_agriculteur",
+                    "exemple_projet",
+                    "etapes",
+                    "eligibilite",
+                    "type_depense",
+                    "types_aides",
+                    "porteurs",
+                    "programmes_parents",
+                    "url_source",
+                    "cibles",
+                    "eligibilite_geographique",
+                    "eligibilite_geographique_exclusions",
+                    "date_ouverture",
+                    "date_cloture",
+                    "date_mise_a_jour",
+                    "base_juridique",
+                    "eligibilite_effectif_minimal",
+                    "eligibilite_effectif_maximal",
+                    "eligibilite_categorie_taille_entreprise",
+                    "eligibilite_annees_existence_minimal",
+                    "eligibilite_forme_juridique",
+                    "eligibilite_forme_juridique_exclusions",
+                    "ciblage_secteur_activite",
+                    "ciblage_naf",
+                    "ciblage_naf_exclusions",
+                    "chainage_paiement",
+                ]
+            elif i == 1:
+                assert len(row) == 31
+                assert row[0] == str(aide.pk)
+                assert row[11] == f"{aide.organisme.nom} (diffuseur)"
+                assert row[15] == "Nationale"
+            else:
+                assert False
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("aide_published__description", ["’≥≤…→‒–ᵉʳ"])
+def test_write_aides_to_non_unicode_file(aide_published):
+    aide = aide_published
+    filename = ""
+    with tempfile.NamedTemporaryFile(
+        "w", encoding="iso8859_15", delete=False, delete_on_close=False
+    ) as f:
+        write_aides_as_csv(f, AideToExternalSchemaForHumans, [aide.pk])
+        filename = f.name
+    with open(filename, "r") as f:
+        for i, row in enumerate(csv.reader(f)):
+            if i == 1:
+                assert row[3] == "'>=<=...->--er"
