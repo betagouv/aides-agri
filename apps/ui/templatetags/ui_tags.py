6de342606072d448de61561f67b01e91279628a8
David Guillot <david.guillot@beta.gouv.fr>
2026-09-23 12:42:45 +0200

    feat(ui): extend dsfr_md to handle tables as multiline

--- apps/ui/templatetags/ui_tags.py
+++ apps/ui/templatetags/ui_tags.py
@@ -1,5 +1,14 @@
 from django import template
+from django.utils.safestring import mark_safe
+from dsfr.extras.markdown.templatetags.dsfr_md_tags import (
+    DsfrTableProcessor,
+    DsfrExtension,
+)
 from dsfr.utils import parse_tag_args
+from markdown import markdown
+from markdown.extensions import Extension
+from markdown.extensions.attr_list import AttrListExtension
+from markdown.extensions.nl2br import Nl2BrExtension
 
 
 register = template.Library()
@@ -61,3 +70,33 @@ def ui_select_rich_multi(*args, **kwargs) -> dict:
         ]
 
     return {"self": tag_data}
+
+
+class UiMultilineTableProcessor(DsfrTableProcessor):
+    def run(self, parent, *args):
+        super().run(parent, *args)
+        div = parent.find("div")
+        div.attrib["class"] += " fr-table--multiline"
+
+
+class UiExtension(Extension):
+    def extendMarkdown(self, md):
+        md.parser.blockprocessors.register(
+            UiMultilineTableProcessor(md.parser, self.getConfigs()), "table", 200
+        )
+
+
+@register.filter(is_safe=True)
+def ui_md(content: str) -> str:
+    return mark_safe(  # nosec B308, B703 — output sanitized by pymdownx.striphtml extension
+        markdown(
+            content,
+            extensions=[
+                AttrListExtension(),
+                DsfrExtension(),
+                UiExtension(),
+                Nl2BrExtension(),
+                "pymdownx.striphtml",
+            ],
+        )
+    )
